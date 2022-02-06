170a047d494dfd905204eb110ae88c472d988c0d
GIO <kge91186@mail.ru>
2022-02-06 23:24:27 +0300

    3 Adding tests should be login_link and suld go to login page

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,8 +1,18 @@
+from selenium.common.exceptions import NoSuchElementException
 
 class BasePage:
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)   
+
+    def is_element_present(self, how, what):  #now we can use this method for cheking wether this element is present in this page or not
+        try:
+            self.browser.find_element(how, what)
+
+        except NoSuchElementException:
+            return False
+        return True        
